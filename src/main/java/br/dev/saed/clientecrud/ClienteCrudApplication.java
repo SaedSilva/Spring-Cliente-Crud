97d0c4d57259436fd6601fa3d7f263a26e708a87
Saed Sousa <saed.sousa@hotmail.com>
2024-06-25 13:06:02 -0300

    Initial Commit

---
+++ src/main/java/br/dev/saed/clientecrud/ClienteCrudApplication.java
@@ -0,0 +1,13 @@
+package br.dev.saed.clientecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteCrudApplication.class, args);
+    }
+
+}
